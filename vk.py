39b48faa079d9501ce3290efea7b6656260c2d4c
Hertuno <hatiandroid@gmail.com>
2020-02-10 17:29:05 +0500

    setting final

--- vk.py
+++ vk.py
@@ -8,11 +8,9 @@ try:
     vk_session.auth(token_only=True)
 except vk_api.AuthError as error_msg:
     print(error_msg)
-print("Finish")
-
 #from vk_api.longpoll import VkLongPoll, VkEventType
 #longpoll = VkLongPoll(vk_session)
 #vk = vk_session.get_api()
 #for event in longpoll.listen():
 #    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-##            vk.messages.send(user_id=event.user_id, message=('Зачем ты пишешь - ' + event.text + '?'));
+#            vk.messages.send(user_id=event.user_id, message=('Зачем ты пишешь - ' + event.text + '?'));
