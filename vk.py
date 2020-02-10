734dac43b2891f9f962be6f671f77d514dd8fd78
Hertuno <hatiandroid@gmail.com>
2020-02-10 18:03:23 +0500

    api

--- vk.py
+++ vk.py
@@ -1,6 +1,6 @@
 import vk_api
 import requests
-
+apiKey = 'b22cdf53821a1fdc8aaa4b1c122c4fbada187ea27623df0c5c993bed9c001f11f40ff22573552b51c3923'
 session = requests.Session()
 login, password = '+79220954334', '031219919vk.com'
 vk_session = vk_api.VkApi(login, password)
@@ -11,6 +11,7 @@ except vk_api.AuthError as error_msg:
 #from vk_api.longpoll import VkLongPoll, VkEventType
 #longpoll = VkLongPoll(vk_session)
 #vk = vk_session.get_api()
+
 #for event in longpoll.listen():
 #    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
 #            vk.messages.send(user_id=event.user_id, message=('Зачем ты пишешь - ' + event.text + '?'));
